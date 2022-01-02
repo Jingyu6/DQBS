159bfa84b832a168a44353ed68106446c8516931
YILEI TU <39905854+abuzhidaoa8@users.noreply.github.com>
2022-01-02 23:12:53 +0100

    Formatted.

--- run.py
+++ run.py
@@ -1,25 +1,25 @@
+# coding=utf-8
 import gym
-import torch
-import numpy as np
 import matplotlib.pyplot as plt
-
-from core.algorithms import DQN, BacktrackDQN, MultiBatchDQN, BacktrackSarsaDQN
+import numpy as np
+import torch
+from core.algorithms import BacktrackDQN, BacktrackSarsaDQN, DQN, MultiBatchDQN
 
 ALGO_NAMES = ['BacktrackSarsaDQN', 'DQN', 'MultiBatchDQN', 'BacktrackDQN']
 ENV_NAME = 'CartPole-v0'
 LOG_INTERVAL = 10
 
 PARAMS = dict(
-    lr=2e-3,
-    gamma=0.99,
-    buffer_size=1e5,
-    sample_size=64,
-    eps_start=0.8,
-    eps_end=0.05,
-    eps_decay=0.95,
-    backtrack_steps=3,
-    use_double_dqn=True
-)
+	lr=2e-3,
+	gamma=0.99,
+	buffer_size=1e5,
+	sample_size=64,
+	eps_start=0.8,
+	eps_end=0.05,
+	eps_decay=0.95,
+	backtrack_steps=3,
+	use_double_dqn=True
+	)
 
 MAX_HORIZON = 10000
 NUM_EPOCHS = 200
@@ -29,64 +29,71 @@ env = gym.make(ENV_NAME)
 STATE_DIM = env.observation_space.shape[0]
 ACTION_DIM = env.action_space.n
 
+
 def main():
-    records = {}
+	records = {}
 
-    for algo_name in ALGO_NAMES:
-        records[algo_name] = [[] for _ in range(len(SEED_LIST))]
+	for algo_name in ALGO_NAMES:
+		records[algo_name] = [[] for _ in range(len(SEED_LIST))]
 
-        for seed_idx, seed in enumerate(SEED_LIST):
-            env.seed(seed)
-            torch.manual_seed(seed)
+		for seed_idx, seed in enumerate(SEED_LIST):
+			env.seed(seed)
+			torch.manual_seed(seed)
 
-            print('Env Name: %s | Seed: %d | STATE_DIM: %d | ACTION_DIM: %d | Algo: %s '
-                  % (ENV_NAME, seed, STATE_DIM, ACTION_DIM, algo_name))
+			print(
+				'Env Name: %s | Seed: %d | STATE_DIM: %d | ACTION_DIM: %d | Algo: %s '
+				% (ENV_NAME, seed, STATE_DIM, ACTION_DIM, algo_name)
+				)
 
-            if algo_name == 'DQN':
-                model = DQN(STATE_DIM, ACTION_DIM, **PARAMS)
-            elif algo_name == 'BacktrackDQN':
-                model = BacktrackDQN(STATE_DIM, ACTION_DIM, **PARAMS)
-            elif algo_name == 'MultiBatchDQN':
-                model = MultiBatchDQN(STATE_DIM, ACTION_DIM, **PARAMS)
-            elif algo_name == 'BacktrackSarsaDQN':
-                model = BacktrackSarsaDQN(STATE_DIM, ACTION_DIM, **PARAMS)
-            else:
-                raise NotImplementedError
+			if algo_name == 'DQN':
+				model = DQN(STATE_DIM, ACTION_DIM, **PARAMS)
+			elif algo_name == 'BacktrackDQN':
+				model = BacktrackDQN(STATE_DIM, ACTION_DIM, **PARAMS)
+			elif algo_name == 'MultiBatchDQN':
+				model = MultiBatchDQN(STATE_DIM, ACTION_DIM, **PARAMS)
+			elif algo_name == 'BacktrackSarsaDQN':
+				model = BacktrackSarsaDQN(STATE_DIM, ACTION_DIM, **PARAMS)
+			else:
+				raise NotImplementedError
 
-            running_reward = 0
-            for i_episode in range(NUM_EPOCHS):
-                state = env.reset()
-                ep_reward = 0
-                for t in range(1, MAX_HORIZON):
-                    action = model.select_action(state)
-                    next_state, reward, done, _ = env.step(action)
-                    model.save_transition(state, action, reward, next_state, done)
-                    state = next_state
-                    ep_reward += reward
-                    model.update()
-                    if done:
-                        break
+			running_reward = 0
+			for i_episode in range(NUM_EPOCHS):
+				state = env.reset()
+				ep_reward = 0
+				for t in range(1, MAX_HORIZON):
+					action = model.select_action(state)
+					next_state, reward, done, _ = env.step(action)
+					model.save_transition(state, action, reward, next_state, done)
+					state = next_state
+					ep_reward += reward
+					model.update()
+					if done:
+						break
 
-                model.end_episode()
-                running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
-                if i_episode % LOG_INTERVAL == 0:
-                    print('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(
-                    i_episode, ep_reward, running_reward))
+				model.end_episode()
+				running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
+				if i_episode % LOG_INTERVAL == 0:
+					print(
+						'Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(
+							i_episode, ep_reward, running_reward
+							)
+						)
 
-                    records[algo_name][seed_idx].append(running_reward)
+					records[algo_name][seed_idx].append(running_reward)
 
-    for algo_name in ALGO_NAMES:
-        data = np.array(records[algo_name])
+	for algo_name in ALGO_NAMES:
+		data = np.array(records[algo_name])
 
-        y_mean = np.mean(data, axis=0)
-        y_std = np.std(data, axis=0)
-        x = range(len(y_mean))
+		y_mean = np.mean(data, axis=0)
+		y_std = np.std(data, axis=0)
+		x = range(len(y_mean))
 
-        plt.plot(x, y_mean)
-        plt.fill_between(x, y_mean - y_std, y_mean + y_std, interpolate=True, alpha=0.3)
+		plt.plot(x, y_mean)
+		plt.fill_between(x, y_mean - y_std, y_mean + y_std, interpolate=True, alpha=0.3)
 
-    plt.legend(ALGO_NAMES)
-    plt.show()
+	plt.legend(ALGO_NAMES)
+	plt.show()
+
 
 if __name__ == '__main__':
-    main()
+	main()
