48403d5b9bdd8c527ca106acb305e76bcf805183
YILEI TU <39905854+abuzhidaoa8@users.noreply.github.com>
2022-01-03 22:37:20 +0100

    Plotted.

--- plot_backstep_priority.py
+++ plot_backstep_priority.py
@@ -1,13 +1,17 @@
+# coding=utf-8
 import argparse
+import os
+import pickle
+import random
+import time
+from multiprocessing import Pool
+
 import gym
 import matplotlib.pyplot as plt
 import numpy as np
-import os.path
-import random
-import time
 import torch
-from core.algorithms import BacktrackDQN, BacktrackSarsaDQN, DQN, MultiBatchDQN
-from multiprocessing import Pool
+
+from core.algorithms import BacktrackSarsaDQN
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--env", "--env", type=str, default='cartpole', choices=['mountaincar', 'cartpole', 'acrobot'])
@@ -19,42 +23,29 @@ parser.add_argument("--sample_size", "--sample_size", type=int, default=64)
 parser.add_argument("--eps_start", "--eps_start", type=float, default=0.8)
 parser.add_argument("--eps_end", "--eps_end", type=float, default=0.05)
 parser.add_argument("--eps_decay", "--eps_decay", type=float, default=0.95)
-parser.add_argument("--backtrack_steps", "--backtrack_steps", type=int, default=3)
+# parser.add_argument("--backtrack_steps", "--backtrack_steps", type=int, default=3)
 parser.add_argument("--use_prioritized_buffer", "--use_prioritized_buffer", action="store_true")
-parser.add_argument("--beta", "--beta", type=float, default=0.5)
+parser.add_argument("--alpha", "--alpha", type=float, default=0.5)
 parser.add_argument("--beta", "--beta", type=float, default=1e-2)
 parser.add_argument("--num_workers", "--num_workers", type=int, default=5)
 args = parser.parse_args()
 
-ALGOS = [DQN, BacktrackDQN, MultiBatchDQN, BacktrackSarsaDQN]
-ALGO_NAMES = [clz.__name__ for clz in ALGOS]
-
-if args.env == 'mountaincar':
-	ENV_NAME = 'MountainCar-v0'
-elif args.env == 'cartpole':
-	ENV_NAME = 'CartPole-v0'
-else:
-	ENV_NAME = 'Acrobot-v1'
-
 PARAMS = vars(args)
-
-LOG_INTERVAL = 5
+ALGO_NAME = 'BacktrackSarsaDQN'
+# ENV_NAMES = ['CartPole-v0', 'MountainCar-v0', 'Acrobot-v1']
+ENV_NAME = "MountainCar-v0"
+# ENV_NAMES = ['MountainCar-v0']
+LOG_INTERVAL = 10
 MAX_HORIZON = 10000
 RUNNING_AVG_WEIGHT = 0.3
+BACKSTEPS = [1, 3, 5, 10]
+SEED_LIST = [227, 222, 1003, 1123]
+PARAMS['num_workers'] = 16
+PARAMS["use_prioritized_buffer"] = True
+
 USE_EVAL_REWARDS = True
 USE_RUNNING_AVG = True
-SET_VERBOSE = False
-NUM_EPOCHS = args.epochs
-SEED_LIST = [227, 222, 1003, 1123, 101]
-
-PLOT_NAME = 'pri={}_lr={}_buffer={}_bstep={}_eps={}_env={}.svg'.format(
-	PARAMS['use_prioritized_buffer'],
-	PARAMS['lr'],
-	PARAMS['buffer_size'],
-	PARAMS['backtrack_steps'],
-	'(' + str(PARAMS['eps_start']) + '|' + str(PARAMS['eps_end']) + '|' + str(PARAMS['eps_decay']) + ')',
-	PARAMS['env']
-	)
+SET_VERBOSE = True
 
 
 def set_seed(env, seed):
@@ -64,26 +55,38 @@ def set_seed(env, seed):
 	random.seed(seed)
 
 
-def worker(seed, algo, algo_name):
+def worker(seed, backstep):
 	""" Run experiments for a specific algorithm with PARAMS with seed """
-	print('Algo: {}, seed: {} has starts.'.format(algo_name, seed))
 	st = time.time()
 
 	env = gym.make(ENV_NAME)
 	set_seed(env, seed)
 
+	if ENV_NAME == "MountainCar-v0":
+		PARAMS["epochs"] = 400
+		PARAMS["buffer_size"] = 1e4
+		PARAMS["lr"] = 1e-4
+
+	NUM_EPOCHS = args.epochs
 	STATE_DIM = env.observation_space.shape[0]
 	ACTION_DIM = env.action_space.n
 
+	PARAMS["backtrack_steps"] = backstep
+
+	print(
+		'\nEnv Name: %s | Seed: %d | State Dim: %d | Action Dim: %d | Back Step: %s has starts.'
+		% (ENV_NAME, seed, STATE_DIM, ACTION_DIM, backstep)
+		)
+
 	records = []
-	model = algo(STATE_DIM, ACTION_DIM, **PARAMS)
+	model = BacktrackSarsaDQN(STATE_DIM, ACTION_DIM, **PARAMS)
 	training_running_reward = 0
 	eval_running_reward = 0
 
 	for i_episode in range(NUM_EPOCHS):
 		state = env.reset()
 		ep_reward = 0
-		for t in range(1, MAX_HORIZON + 1):
+		for _ in range(1, MAX_HORIZON + 1):
 			action = model.select_action(state)
 			next_state, reward, done, _ = env.step(action)
 			model.save_transition(state, action, reward, next_state, done)
@@ -107,50 +110,59 @@ def worker(seed, algo, algo_name):
 				)
 			if SET_VERBOSE:
 				print(
-					'Algo: {}, Seed: {}, Episode {}\tLast reward: {:.2f}\tRunning training reward: {:.2f}\tEvaluation reward: {:.2f}\tRunning eval reward: '.format(
-						algo_name, seed, i_episode, ep_reward, training_running_reward, evaluation_reward,
+					'Algo: {}, Seed: {}, Episode {}\tLast reward: {:.2f}\tRunning training reward: {:.2f}\tEvaluation reward: {:.2f}\tRunning eval reward: {:.2f}'.format(
+						ALGO_NAME, seed, i_episode, ep_reward, training_running_reward, evaluation_reward,
 						eval_running_reward
 						)
 					)
 
 	et = time.time()
-	print('Algo: {}, seed: {} has finished, elapsed time: {:2.4f}s.'.format(algo_name, seed, et - st))
+	print(
+		'\nEnv Name: {} | Seed: {} | State Dim: {} | Action Dim: {} | Back Step: {} has finished, elapsed time: {:2.4f}s.'
+			.format(ENV_NAME, seed, STATE_DIM, ACTION_DIM, backstep, et - st)
+		)
+
 	return records
 
 
 def main():
-	print('Experiment hyperparameters: ', PARAMS)
-
 	records = {}
 	arguments = []
 
-	for algo, algo_name in zip(ALGOS, ALGO_NAMES):
+	for backstep in BACKSTEPS:
 		for seed in SEED_LIST:
-			arguments.append([seed, algo, algo_name])
+			arguments.append([seed, backstep])
 
 	with Pool(PARAMS['num_workers']) as p:
 		return_results = p.starmap(worker, arguments)
 
-	for (_, _, algo_name), record in zip(arguments, return_results):
-		if algo_name not in records:
-			records[algo_name] = []
-		records[algo_name].append(record)
+	for (_, backstep), record in zip(arguments, return_results):
+		if backstep not in records:
+			records[backstep] = []
+		records[backstep].append(record)
 
-	for algo_name in ALGO_NAMES:
-		data = np.array(records[algo_name])
+		records_file = open('result/backstep/pri/record_backstep_{}.pickle'.format(ENV_NAME), 'wb')
+		pickle.dump(records, records_file)
+		records_file.close()
+
+
+def plot_backstep_by_env():
+	with open('result/backstep/pri/record_backstep_{}.pickle'.format(ENV_NAME), 'rb') as file:
+		records = pickle.load(file)
+
+	for backstep in BACKSTEPS:
+		data = np.array(records[backstep])
 
 		y_mean = np.mean(data, axis=0)
 		y_std = np.std(data, axis=0)
-		x = [int((epoch + 1) * 10) for epoch in range(len(y_mean))]
+		x = [(epoch + 1) * LOG_INTERVAL for epoch in range(len(y_mean))]
 
 		plt.plot(x, y_mean)
 		plt.fill_between(x, y_mean - y_std, y_mean + y_std, interpolate=True, alpha=0.3)
 
-	plt.legend(ALGO_NAMES)
-	plt.savefig(os.path.join('result', PLOT_NAME))
-
-
-# plt.show()
+	plt.legend(["backstep = {}".format(backstep) for backstep in BACKSTEPS])
+	plt.savefig(os.path.join('result/backstep/pri/', 'Backstep_{}.svg'.format(ENV_NAME)))
+	plt.clf()
 
 
 def evaluate_model(env, model, episodes=5, gamma=0.999):
@@ -169,3 +181,4 @@ def evaluate_model(env, model, episodes=5, gamma=0.999):
 
 if __name__ == '__main__':
 	main()
+	plot_backstep_by_env()
