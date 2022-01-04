67ff988ceb75d5e2e7ce01bd7b89973a761c3c66
Jingyu6 <liujingyu@student.ethz.ch>
2022-01-04 15:42:02 +0100

    Modified run scripts

--- scripts/run_acrobot.sh
+++ scripts/run_acrobot.sh
@@ -1,5 +1,5 @@
 # !/bin/bash
 
 echo 'Start running experiments for acrobot'
-python run.py --lr=5e-4 --buffer_size=1e3
-python run.py --lr=1e-4 --buffer_size=1e3 --use_prioritized_buffer --beta=1e-4 --alpha=0.4
+python run.py --env=acrobot --lr=5e-4 --buffer_size=1e3
+python run.py --env=acrobot --lr=1e-4 --buffer_size=1e3 --use_prioritized_buffer --beta=1e-4 --alpha=0.4
